8a2ed931c8091cfa601ce9484793b2d85e161967
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-06-17 04:58:26 +0300

    Add Find Peak Element problem

---
+++ ProplemSolving/LeetCodeProblems/FindPeakElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    
+    public class FindPeakElement
+    {
+        public int PeakElement { get; set;}
+        public int BinarySearch(int[] nums, int left, int right)
+        {
+            if (left == right)
+                return left;
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid + 1])
+                return BinarySearch(nums, left, mid);
+            return BinarySearch(nums, mid + 1, right);
+        }
+
+        public FindPeakElement(int[] nums)
+        {
+            PeakElement = BinarySearch(nums, 0, nums.Length - 1);
+        }
+    }
+}
